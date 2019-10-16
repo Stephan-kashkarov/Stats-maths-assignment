1b7e925af28a1b6848e573a1fc7558ff73595f63
stephan-kashkarov <kashkarovwork@gmail.com>
2019-10-16 21:55:22 +1100

    added hist code

--- A.py
+++ A.py
@@ -1,3 +1,17 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+
+def distribution_graph(series, title='distribution'):
+    plt.title(title)
+    plt.hist(series.to_numpy(), bins=100)
+    plt.show()
+
+data = pd.read_csv('data/climate.csv')
+
+for key in data.keys()[1:]:
+    print(key)
+    # try:
+    distribution_graph(data[key], title=key)
+    # except:
+    #     pass
