2b895e583716a429737825cfad6b7f57d42bc38e
stephan-kashkarov <kashkarovwork@gmail.com>
2019-10-20 19:26:08 +1100

    working on custom fitting

--- A.py
+++ A.py
@@ -1,25 +1,41 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import scipy.stats
+from scipy.stats import exponnorm as f, describe
+import scipy.special as sse
+from scipy.optimize import curve_fit
     
+#pylint: disable=no-member
+f = lambda x, l, s, m: 0.5*l*np.exp(0.5*l*(2*m+l*s*s-2*x))*sse.erfc((m+l*s*s-x)/(np.sqrt(2)*s))
 
 data = pd.read_csv('data/cars.csv')
-key = 'year'
-d = data[key].dropna()
 
-print(scipy.stats.describe(d))
+d = np.array(filter(lambda x: int(x) >= 1950, data['year'].dropna().to_numpy()))
 
-series = d.to_numpy()
-# plt.hist(series, bins=120)
+print(describe(d))
+
 # plt.show()
+bins = (2020 - 1950)
 
-hist = np.histogram(series, bins=120)
-print(len(hist))
+params = f.fit(d)[1:]
+print(params)
+data_range = range(1950, 2021)
+pdf = f.pdf(data_range, 1.5, *params)
+fig, ax1 = plt.subplots()
+ax1.margins(x=0, y=0)
+ax1.set_xlabel('Year')
+ax1.set_ylabel('Count', color="tab:blue")
+ax1.hist(d, bins=bins)
 
-dist = scipy.stats.exponnorm.fit(hist, 1.5)
+ax2 = ax1.twinx()
 
-print(dist)
-plt.plot(range(1900, 2021), [dist.pdf(x) for x in range(1900, 2021)])
+color = 'tab:red'
+ax2.margins(x=0, y=0)
+ax2.set_ylabel('Probalility', color=color)
+ax2.plot(data_range, pdf, color=color)
+ax2.tick_params(axis='y', labelcolor=color)
 
+
+# plt
+fig.tight_layout()
 plt.show()
