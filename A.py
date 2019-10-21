58de30444bcca17dfdf33868f5049a07ba1fa127
stephan-kashkarov <kashkarovwork@gmail.com>
2019-10-21 20:33:32 +1100

    made a question B analysis script

--- A.py
+++ A.py
@@ -6,28 +6,30 @@ import scipy.special as sse
 from scipy.optimize import curve_fit
     
 #pylint: disable=no-member
-f = lambda x, l, s, m: 0.5*l*np.exp(0.5*l*(2*m+l*s*s-2*x))*sse.erfc((m+l*s*s-x)/(np.sqrt(2)*s))
+# f = lambda x, l, s, m: 0.5*l*np.exp(0.5*l*(2*m+l*s*s-2*x))*sse.erfc((m+l*s*s-x)/(np.sqrt(2)*s))
 
 data = pd.read_csv('data/cars.csv')
 
-d = np.array(filter(lambda x: int(x) >= 1950, data['year'].dropna().to_numpy()))
-
-print(describe(d))
+d = list(filter(lambda x: int(x) >= 1950, data['year'].dropna().to_numpy()))
+print(len(d))
+# print(describe(d))
 
 # plt.show()
 bins = (2020 - 1950)
 
-params = f.fit(d)[1:]
+params = f.fit(d)
 print(params)
-data_range = range(1950, 2021)
-pdf = f.pdf(data_range, 1.5, *params)
 fig, ax1 = plt.subplots()
 ax1.margins(x=0, y=0)
 ax1.set_xlabel('Year')
 ax1.set_ylabel('Count', color="tab:blue")
+ax1.tick_params(axis='y', labelcolor="tab:blue")
 ax1.hist(d, bins=bins)
 
 ax2 = ax1.twinx()
+
+data_range = range(1950, 2021)
+pdf = f.pdf(data_range, *params)
 
 color = 'tab:red'
 ax2.margins(x=0, y=0)
---
+++ B.py
@@ -0,0 +1,56 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import scipy.stats as ss
+from scipy.optimize import curve_fit
+
+data1 = pd.read_csv('data/rain_data1.csv').sort_values(by=['year'])
+data2 = pd.read_csv('data/rain_data2.csv').sort_values(by=['year'])
+
+func = lambda x, m, b: (m * x) + b
+
+print(data1.keys())
+print_data = lambda title, d: print(
+f"""{title} - my non excel analysis pack
+ - mean:               {d.mean()}
+ - median:             {d.median()}
+ - standard deviation: {d.std()}
+ - skew:               {d.skew()}
+ - kurtosis:           {d.kurtosis()}""")
+
+print("Temperature:")
+print_data("Temp - part 1", data1['temperature'])
+print_data("Temp - part 2", data2['temperature'])
+
+print("\nRainfall:")
+print_data("Rain - part 1", data1['rainfall'])
+print_data("Rain - part 2", data2['rainfall'])
+
+def plot(ax, d, key):
+    s1 = d['year'].to_numpy()
+    s2 = d[key].to_numpy()
+    ax.plot(s1, s2, ".")
+
+def qq(ax, d1, key, title):
+    ss.probplot(d1[key].to_numpy(), dist="norm", plot=ax)
+    ax.set_title(title)
+
+
+types = ["temperature", "rainfall"]
+# for key in types:
+#     fig, (ax1, ax2) = plt.subplots(2, 1)
+#     for ax, key2, data in [(ax1, "part 1", data1), (ax2, "part 2", data2)]:
+#         ax.set_title(key2)
+#         plot(ax, data, key)
+#     fig.suptitle(f"{key} - data")
+#     plt.show()
+fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2)
+fig.suptitle(f"Q-Q plots")
+for key, axes in zip(types, [(ax1, ax2), (ax3, ax4)]):
+    for ax, key2, data in [(axes[0], "part 1", data1), (axes[1], "part 2", data2)]:
+        qq(ax, data, key, f"{key} - {key2}")
+plt.show()
+
+
+
+
